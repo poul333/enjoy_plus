50a56cd2af252e9d118169ff1822f7ed1d00c7f5
poul <2978688528@qq.com>
2023-01-16 21:05:24 +0800

    feat(user):查询周边小区

--- miniprogram/house_pkg/pages/locate/index.js
+++ miniprogram/house_pkg/pages/locate/index.js
@@ -1,66 +1,53 @@
-// house_pkg/pages/locate/index.ts
+import qqMap from '../../../utils/qqmap'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad() {
-
+    this.getLocation()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  async getLocation() {
+    const { latitude, longitude } = await wx.getLocation()
+    this.getPoint(latitude, longitude)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  async chooseLocation() {
+    const { latitude, longitude } = await wx.chooseLocation()
+    this.getPoint(latitude, longitude);
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+  getPoint(latitude, longitude) {
 
-  },
+    wx.showLoading({ title: '正在加载', mask: true })
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    //  逆地址解析
+    qqMap.reverseGeocoder({
+      location: [latitude, longitude].join(','),
+      success: ({ result: { address } }) => {
+        this.setData({ address })
+      }
+    }),
 
-  },
+      // 地点搜索
+      qqMap.search({
+        // 搜索关键字（查询周边的小区）
+        keyword: '住宅小区',
+        // 指定经纬度
+        location: [latitude, longitude].join(','),
+        // 查询数据条数
+        page_size: 5,
+        success: (res) => {
+          const points = []
+          res.data.forEach(({ id, title, _distance }) => {
+            points.push({ id, title, _distance })
+          })
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+          this.setData({ points })
+        },
+        complete: () => {
+          wx.hideLoading()
+        }
+      })
 
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
 
   }
 })
---
+++ miniprogram/utils/qqmap.js
@@ -0,0 +1,6 @@
+// 导入 qq  地图sdk 
+import qqMap from '../libs/qqmap-wx-jssdk.min'
+
+export default new qqMap({
+  key: 'O2EBZ-A67C3-HTY3V-3K7Y3-A2U7J-J4BVV'
+});
