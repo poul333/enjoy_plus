3e4c56c3d3e669e8ce6a4a3f008d0fb50310b607
poul <2978688528@qq.com>
2023-01-17 20:02:16 +0800

    feat(repair): 完成报修信息的数据处理

--- miniprogram/repair_pkg/pages/form/index.js
+++ miniprogram/repair_pkg/pages/form/index.js
@@ -1,21 +1,77 @@
+// import dayjs from "dayjs"
+var dayjs = require('dayjs')
+
 Page({
   data: {
     currentDate: new Date().getTime(),
     houseLayerVisible: false,
     repairLayerVisible: false,
     dateLayerVisible: false,
-    houseList: [
-      { name: '北京西三旗花园1号楼 101' },
-      { name: '北京东村家园3号楼 302' },
-      { name: '北京育新花园3号楼 703' },
-      { name: '北京天通苑北苑8号楼 403' },
-    ],
-    repairItem: [{ name: '水路卫浴' }, { name: '电路灯具' }, { name: '管道疏通' }, { name: '开锁换锁' }],
-    fileList: [
-      { url: '/repair_pkg/static/uploads/attachment.jpg' },
-      { url: '/repair_pkg/static/uploads/attachment.jpg' },
-    ],
+    houseList: [],
+    repairItem: [],
+    attachment: [],
   },
+
+  onLoad() {
+    this.getHouseList()
+    this.getRepairItem()
+  },
+
+  // 获取房屋列表
+  async getHouseList() {
+    const { code, data: houseList } = await wx.http.get('/house')
+    if (code !== 10000) return wx.utils.toast()
+    this.setData({ houseList })
+  },
+
+  // 获取维修列表
+  async getRepairItem() {
+    const { code, data: repairItem } = await wx.http.get('/repairItem')
+    if (code !== 10000) return wx.utils.toast()
+    this.setData({ repairItem })
+  },
+
+  // 选择维修房屋
+  selectHouse(ev) {
+    const { name: houseInfo } = ev.detail
+    this.setData({ houseInfo })
+  },
+
+  // 选择维修项目
+  selectRepairItem(ev) {
+    const { name: repairItemName } = ev.detail
+    this.setData({ repairItemName })
+  },
+
+  // 选择维修时间
+  selectDate(ev) {
+    this.setData({
+      dateLayerVisible: false,
+      appointment: dayjs(ev.detail).format('YYYY/MM/DD')
+    })
+  },
+
+  // 上传图片
+  afterRead(ev) {
+    const { file } = ev.detail
+    wx.uploadFile({
+      url: wx.http.baseURL + '/upload',
+      filePath: file.url,
+      name: 'file',
+      header: {
+        Authorization: getApp().token,
+      },
+      success: (res) => {
+        const data = JSON.parse(res.data)
+        // 上传完成需要更新
+        const { attachment } = this.data
+        attachment.push({ ...data.data })
+        // 渲染数据
+        this.setData({ attachment })
+      }
+    })
+  },
+
   openHouseLayer() {
     this.setData({ houseLayerVisible: true })
   },
