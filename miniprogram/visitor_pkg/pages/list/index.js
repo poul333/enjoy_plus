269c17d380ad9c9db66a4a56fb84b20065c50be0
poul <2978688528@qq.com>
2023-01-18 16:09:11 +0800

    feat(visit):完成访客管理

--- miniprogram/visitor_pkg/pages/list/index.js
+++ miniprogram/visitor_pkg/pages/list/index.js
@@ -1,7 +1,19 @@
 Page({
-  goPassport() {
+  goPassport(ev) {
+    const { id, status } = ev.mark
+    if (status === 0) return wx.utils.toast('当前通行证已失效')
     wx.navigateTo({
-      url: '/visitor_pkg/pages/passport/index',
+      url: '/visitor_pkg/pages/passport/index?id=' + id,
     })
   },
+
+  onLoad() {
+    this.getVistorList()
+  },
+
+  async getVistorList() {
+    const { code, data: { rows: visitorList } } = await wx.http.get('/visitor')
+    if (code !== 10000) return wx.utils.toast()
+    this.setData({ visitorList })
+  }
 })
--- miniprogram/visitor_pkg/pages/passport/index.js
+++ miniprogram/visitor_pkg/pages/passport/index.js
@@ -1,4 +1,19 @@
 Page({
+
+  onLoad({ id }) {
+    // 获取通行证
+    this.getPassport(id)
+  },
+  async getPassport(id) {
+    if (!id) return
+    // 请求数据接口
+    const { code, data: passport } = await wx.http.get('/visitor/' + id)
+    // 检测接口调用的结果
+    if (code !== 10000) return wx.utils.toast('获取通行证失败!')
+    // 渲染通行证
+    this.setData({ ...passport })
+  },
+
   onShareAppMessage() {
     return {
       title: '查看通行证',
@@ -6,4 +21,12 @@ Page({
       imageUrl: 'https://enjoy-plus.oss-cn-beijing.aliyuncs.com/images/share_poster.png',
     }
   },
+
+  // 保存图片
+  async saveImage() {
+    // 获取图片的临时路径
+    const { path } = await wx.getImageInfo({ src: this.data.url })
+    // 将图片保存到本地
+    await wx.saveImageToPhotosAlbum({ filePath: path })
+  }
 })
